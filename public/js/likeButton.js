ac96039dd1b9d92f79fa1b37c92749a1158ece28
Stella Maris González Robert <127294546+Stegonyrob@users.noreply.github.com>
2023-10-29 18:19:29 +0100

    heart functional

--- public/js/likeButton.js
+++ public/js/likeButton.js
@@ -2,13 +2,13 @@ var likeBtn = document.getElementById('like-btn');
 var isLiked = localStorage.getItem('isLiked') === 'true';
 
 function handleClick() {
-    isLiked = !isLiked;
-    localStorage.setItem('isLiked', isLiked);
-    likeBtn.classList.toggle('like-active');
+  isLiked = !isLiked;
+  localStorage.setItem('isLiked', isLiked.toString());
+  likeBtn.classList.toggle('clicked');
 }
 
 likeBtn.addEventListener('click', handleClick);
 
 if (isLiked) {
-    likeBtn.classList.add('like-active');
+  likeBtn.classList.add('clicked');
 }
