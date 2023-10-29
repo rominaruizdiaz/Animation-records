a1c28b1909f23e19f405e07d081b36cdce1f7f31
Stella Maris González Robert <127294546+Stegonyrob@users.noreply.github.com>
2023-10-29 18:07:13 +0100

    counter functional and two display white persistence

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,16 +1,33 @@
-let count = 0;
-        const display = document.querySelector('.display');
+var count = 0;
 
-        document.querySelector('.increment').addEventListener('click', () => {
-            if (count < 10) {
-                count++;
-                display.textContent = count;
-            }
-        });
+const display = document.querySelector('.display');
+const displayNav = document.querySelector('.displayNav');
 
-        document.querySelector('.decrement').addEventListener('click', () => {
-            if (count > 0) {
-                count--;
-                display.textContent = count;
-            }
-        });
+function updateDisplay() {
+    display.textContent = count;
+    displayNav.textContent = count;
+}
+
+document.querySelector('.increment').addEventListener('click', () => {
+    if (count < 10) {
+        count++;
+        updateDisplay();
+        localStorage.setItem('count', count);
+    }
+});
+
+document.querySelector('.decrement').addEventListener('click', () => {
+    if (count > 0) {
+        count--;
+        updateDisplay();
+        localStorage.setItem('count', count);
+    }
+});
+
+window.onload = function() {
+    var savedCount = localStorage.getItem('count');
+    if (savedCount) {
+        count = parseInt(savedCount);
+        updateDisplay();
+    }
+}
--- public/js/likeButton.js
+++ public/js/likeButton.js
@@ -1,5 +1,14 @@
-document.getElementById('like-button').addEventListener('click', function() {
-    var icon = this.querySelector('i');
-    icon.classList.toggle('liked');
-  });
-  
+var likeBtn = document.getElementById('like-btn');
+var isLiked = localStorage.getItem('isLiked') === 'true';
+
+function handleClick() {
+    isLiked = !isLiked;
+    localStorage.setItem('isLiked', isLiked);
+    likeBtn.classList.toggle('like-active');
+}
+
+likeBtn.addEventListener('click', handleClick);
+
+if (isLiked) {
+    likeBtn.classList.add('like-active');
+}
