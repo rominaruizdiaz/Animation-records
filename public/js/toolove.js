65ab284a580c0b449dff35ce6004b9394d9f6817
Stella Maris González Robert <127294546+Stegonyrob@users.noreply.github.com>
2023-10-31 17:30:22 +0100

    actualization toolove js

--- public/js/toolove.js
+++ public/js/toolove.js
@@ -1,12 +1,15 @@
 let slideIndex = 0;
 showSlides(slideIndex);
+
 function plusDivs(n) {
     showSlides(slideIndex += n);
 }
+
 function showSlides(n) {
     let slides = document.querySelectorAll(".lovetoo-card-detail");
     let slidesPerPage = 4;
     let startIndex = n * slidesPerPage;
+
     if (startIndex >= slides.length) {
         slideIndex = 0;
         startIndex = 0;
@@ -15,5 +18,12 @@ function showSlides(n) {
         slideIndex = Math.floor((slides.length - 1) / slidesPerPage);
         startIndex = slideIndex * slidesPerPage;
     }
+
+
     $(slides).hide();
+
+
+    for (let i = startIndex; i < startIndex + slidesPerPage && i < slides.length; i++) {
+        $(slides[i]).show();
+    }
 }
